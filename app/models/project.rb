40ec6a2fc8e0816890ce424b31ff00cc50b839ab
Daniel Flores <d.flores042@gmail.com>
2018-06-19 18:26:34 -0400

    implement image defaults for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,18 @@
 class Project < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
-  scope :angular, -> { where(subtitle: 'Angular') }
+# create custom scopes two different ways
+	def self.angular
+		where(subtitle: 'Angular') }
+	end
+
 	scope :ruby_on_rails_projects, -> { where(subtitle: 'Ruby on Rails') }
+
+# add default images if none are provided
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://via.placeholder.com/600x400"
+		self.thumb_image ||= "http://via.placeholder.com/350x200"
+	end
 end
