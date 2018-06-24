0023a50adad8b320ea22f9abb2f4feb553ec77d7
Daniel Flores <d.flores042@gmail.com>
2018-06-24 10:51:50 -0400

    implement basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :projects, except: [:show]
   get 'rails-projects', to: 'projects#rails'
   get 'project/:id', to: 'projects#show', as: 'project_show'
