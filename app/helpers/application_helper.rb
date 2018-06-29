281d4fafb97d131416f3b43d5b8d227d3f3bdab7
Daniel Flores <d.flores042@gmail.com>
2018-06-28 21:32:52 -0400

    add nav bar, add default header  content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		base_title = "Dan Flores"
+		if page_title.empty?
+			base_title
+		else 
+			"#{page_title} | #{base_title}"
+		end
+	end
+
+	def seo_keywords
+		"daniel dan flores portfolio project projects"
+	end
 end
