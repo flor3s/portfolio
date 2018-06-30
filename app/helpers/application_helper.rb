4e47adc96147188125b9dab0fc32cff2049170e3
Daniel Flores <d.flores042@gmail.com>
2018-06-30 13:16:23 -0400

    implement view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,14 @@ module ApplicationHelper
 	def seo_keywords
 		"daniel dan flores portfolio project projects"
 	end
+
+	def login_helper
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, method: :delete 
+    else
+      (link_to "Register", new_user_registration_path) +
+      "<br>".html_safe +
+      (link_to "Login", new_user_session_path)
+    end	
+	end
 end
