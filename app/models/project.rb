cecd1fc7ed6bcdd2a489d6c41962141859f1cb13
Daniel Flores <d.flores042@gmail.com>
2018-10-20 14:53:12 -0400

    implement initial file upload system via carrierwave

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,10 @@ class Project < ApplicationRecord
 	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
+  mount_uploader :main_image, ProjectUploader
+  mount_uploader :thumb_image, ProjectUploader
+
+
 # create custom scopes two different ways
 	def self.angular
 		where(subtitle: 'Angular')
