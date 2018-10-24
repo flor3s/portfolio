2e3f26337dd438e1c4b9da5af86a38f0df8ca98b
Daniel Flores <d.flores042@gmail.com>
2018-10-24 19:51:32 -0400

    integrate cocoon for nested project forms

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,7 +23,6 @@ class ProjectsController < ApplicationController
 
 	def new
 		@project = Project.new
-    3.times { @project.technologies.build }
 	end
 
   def create
@@ -70,7 +69,7 @@ class ProjectsController < ApplicationController
                                       :body,
                                       :main_image,
                                       :thumb_image,
-                                      technologies_attributes: [:name]
+                                      technologies_attributes: [:id, :name, :_destroy]
                                       )
     end
 
